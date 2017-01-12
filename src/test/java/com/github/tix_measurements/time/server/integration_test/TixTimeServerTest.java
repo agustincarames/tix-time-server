96ea78eb24e4b31ed6eae83624535a9156b05f37
Facundo Martinez <facu@spect.ro>
2017-01-12 12:09:08 -0300

    master: 	* Updating tests

--- src/test/java/com/github/tix_measurements/time/server/integration_test/TixTimeServerTest.java
+++ src/test/java/com/github/tix_measurements/time/server/integration_test/TixTimeServerTest.java
@@ -1,72 +1,59 @@
 package com.github.tix_measurements.time.server.integration_test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tix_measurements.time.core.data.TixDataPacket;
 import com.github.tix_measurements.time.core.data.TixPacket;
 import com.github.tix_measurements.time.core.data.TixPacketType;
 import com.github.tix_measurements.time.core.util.TixCoreUtils;
 import com.github.tix_measurements.time.server.TixTimeServer;
-import com.github.tix_measurements.time.server.handler.TixUdpServerHandlerTest;
+import com.github.tix_measurements.time.server.util.jackson.TixPacketSerDe;
 import com.github.tix_measurements.time.server.utils.TestDataUtils;
 import com.rabbitmq.client.*;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.security.KeyPair;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeoutException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TixTimeServerTest {
 
-	private static String QUEUE_HOST;
-	private static String QUEUE_NAME;
-	private static int SERVER_WORKER_THREADS;
-	private static int SERVER_PORT;
+	private String queueHost;
+	private String queueName;
+	private int serverWorkerThreads;
+	private int serverPort;
 
 	private TixTimeTestClient client;
 
 	private TixTimeServer server;
 
-	@BeforeClass
-	public static void classSetup() throws IOException, TimeoutException {
-		QUEUE_HOST = "localhost";
-		QUEUE_NAME = "test-queue-" + RandomStringUtils.randomAlphanumeric(4);
-		SERVER_WORKER_THREADS = Runtime.getRuntime().availableProcessors();
-		SERVER_PORT = RandomUtils.nextInt(1025, (Short.MAX_VALUE * 2) - 1);
-		setupQueue();
+	@Before
+	public void setup() throws IOException, TimeoutException {
+		queueHost = "localhost";
+		queueName = "test-queue-" + RandomStringUtils.randomAlphanumeric(4);
+		serverWorkerThreads = Runtime.getRuntime().availableProcessors();
+		serverPort = RandomUtils.nextInt(1025, (Short.MAX_VALUE * 2) - 1);
+//		setupQueue();
+		this.server = new TixTimeServer(queueHost, queueName, serverWorkerThreads, serverPort);
+		this.client = new TixTimeTestClient(serverPort);
 	}
 
-	private static Channel getChannel() throws IOException, TimeoutException {
+	@After
+	public void teardown() throws IOException, TimeoutException {
+		Channel channel = getChannel();
+		channel.queueDelete(queueName);
+	}
+
+	private Channel getChannel() throws IOException, TimeoutException {
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(QUEUE_HOST);
+		factory.setHost(queueHost);
 		return factory.newConnection()
 					.createChannel();
-	}
-
-	private static void setupQueue() throws IOException, TimeoutException {
-		Channel channel = getChannel();
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	}
-
-	@AfterClass
-	public static void classTeardown() throws IOException, TimeoutException {
-		Channel channel = getChannel();
-		channel.queueDelete(QUEUE_NAME);
-	}
-
-	@Before
-	public void setup() {
-		this.server = new TixTimeServer(QUEUE_HOST, QUEUE_NAME, SERVER_WORKER_THREADS, SERVER_PORT);
-		this.client = new TixTimeTestClient(SERVER_PORT);
 	}
 
 	private void testPacket(TixPacket packet) throws InterruptedException {
@@ -101,5 +88,24 @@ public class TixTimeServerTest {
 		TixPacket packet = new TixPacket(this.client.getClientAddress(), this.client.getServerAddress(),
 				TixPacketType.LONG, TixCoreUtils.NANOS_OF_DAY.get());
 		testPacket(packet);
+	}
+
+	@Test
+	public void testSendDataPacket() throws InterruptedException, IOException, TimeoutException {
+		byte[] message = TestDataUtils.INSTANCE.generateMessage();
+		TixPacket dataPacket = new TixDataPacket(this.client.getClientAddress(), this.client.getServerAddress(),
+				TixCoreUtils.NANOS_OF_DAY.get(), TestDataUtils.INSTANCE.getPublicKey(), message,
+				TestDataUtils.INSTANCE.getSignature(message));
+		testPacket(dataPacket);
+		Channel channel = getChannel();
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+				TixPacketSerDe serde = new TixPacketSerDe();
+				TixDataPacket receivedPacket = serde.deserialize(body);
+				assertThat(receivedPacket).isEqualTo(dataPacket);
+			}
+		};
+		channel.basicConsume(queueName, true, consumer);
 	}
 }
